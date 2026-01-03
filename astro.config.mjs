ae7d4850ff744cac4ca183c92800627808f4290d
Welile Zwane <welilezwane@MacBook-Pro.local>
2026-01-03 12:33:04 +0200

    adding deployment settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  site: 'https://www.wzwane.github.io',
+  base: '/handcrafted',
+   trailingSlash: "never"
 });
